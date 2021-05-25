470b834791da7bb233904b3e107fca489c03ea21
Logan Robinson <loganrobinson315@gmail.com>
2021-05-25 17:21:55 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import Header from './components/Header'
+import NavBar from './components/NavBar'
 
 function App () {
   return (
-    <div className='App'>
-      app
+    <div className='h-full'>
+      <NavBar />
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header () {
+  return (
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border-2 border-black h-screen items-center justify-center md:flex hidden'>
+      <p>Hello World</p>
+
+    </div>
+  )
+}
