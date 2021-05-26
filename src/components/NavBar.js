f0e2bd548deaec24b98fd60b663b9c287f6c39d9
Daniel McKinney <adrmckinney@gmail.com>
2021-05-26 00:11:03 -0400

    added 'fixed' to nav bar so that it would stick around, making it easier for us to see the window breakpoint debugger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,7 +28,7 @@ function classNames (...classes) {
 
 export default function NavBar () {
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure as='nav' className='bg-gray-800 fixed top-0 w-full'>
       {({ open }) => (
         <>
           <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
@@ -38,11 +38,13 @@ export default function NavBar () {
                   {/* Mobile menu button */}
                   <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
                     <span className='sr-only'>Open main menu</span>
-                    {open ? (
-                      <XIcon className='block h-6 w-6' aria-hidden='true' />
-                    ) : (
-                      <MenuIcon className='block h-6 w-6' aria-hidden='true' />
-                    )}
+                    {open
+                      ? (
+                        <XIcon className='block h-6 w-6' aria-hidden='true' />
+                        )
+                      : (
+                        <MenuIcon className='block h-6 w-6' aria-hidden='true' />
+                        )}
                   </Disclosure.Button>
                 </div>
                 {/* <div className='flex-shrink-0 flex items-center'>
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -47,11 +47,11 @@ const VIDEOS = [
 
 export default function VideoCard () {
   return (
-    <ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2'>
+    <ul className='grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2 mt-4'>
       {VIDEOS.map((video, idx) => (
         <li
           key={idx}
-          className='col-span-1 bg-white rounded-lg w-full'
+          className='col-span-1 bg-white rounded-lg w-full flex justify-center'
         >
           <iframe id='ytplayer' title={video.title} src={video.videoUrl} frameborder='0' width='480' height='270' type='text/html' />
         </li>
