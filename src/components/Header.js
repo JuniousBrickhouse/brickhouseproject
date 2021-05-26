503610e5298616f390d6807b9bc825ad3367d1cb
Logan Robinson <loganrobinson315@gmail.com>
2021-05-26 16:35:23 -0400

    added colors and fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 export default function Header () {
   return (
-    <div className=' mx-auto px-4 sm:px-6 lg:px-8 border-2 h-4/5 w-screen items-center justify-center xl:flex bg-junious-headshot bg-top bg-cover xl:bg-top' />
+    <div className=' mx-auto px-4 sm:px-6 lg:px-8 border-2 h-4/5 w-screen items-center justify-center xl:flex bg-junious-headshot sm:bg-top bg-cover xl:bg-top bg-center' />
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,30 @@ module.exports = {
         // 'headshot-junious': "url('./photos/headshot.jpeg')"
         // 'footer-texture': "url('/img/footer-texture.png')"
 
-      })
+      }),
+      colors: {
+        richBlack: '#0F0F0F',
+        daviesGrey: '#4E4E4E',
+        transparent: 'transparent',
+        current: 'currentColor',
+        spanishGrey: '#9C9C9C',
+        sonicSilver: '#707070',
+        platinum: '#E6E6E6',
+        indigoDye: '#25426B',
+        yNBlue: '#314C7C',
+        glaucous: '#6C8CC7',
+        wildBlueYonder: '#9EB1D6',
+        burlyWood: '#E2BC8A',
+        earthYellow: '#D1A15D',
+        fawn: '#D99F68',
+        mediumCarmine: '#AA563B'
+
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+        nunito: ['Nunito'],
+        allura: ['Allura']
+      }
     }
   },
   variants: {
