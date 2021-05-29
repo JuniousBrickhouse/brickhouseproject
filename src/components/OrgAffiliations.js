3799b334fc7db27ca8123bef27defa5c68aa6d72
Daniel McKinney <adrmckinney@gmail.com>
2021-05-29 15:31:24 -0400

    changed color of card bgs of hats

--- src/components/OrgAffiliations.js
+++ src/components/OrgAffiliations.js
@@ -5,11 +5,8 @@ const POSTS = [
     category: { name: 'Article', href: '#' },
     description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
     imageUrl:
-        'https://img1.wsimg.com/isteam/ip/93b63956-2d59-47b1-8349-dfc27f026e57/23rd%20%26%20Hampton%20Logo%201b%20copy.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1320,h:1000,cg:true,m/cr=w:1320,h:1000,a:cc',
-    readingTime: '6 min'
+        'https://img1.wsimg.com/isteam/ip/93b63956-2d59-47b1-8349-dfc27f026e57/23rd%20%26%20Hampton%20Logo%201b%20copy.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1320,h:1000,cg:true,m/cr=w:1320,h:1000,a:cc'
   },
   {
     title: 'Next Level',
@@ -17,11 +14,8 @@ const POSTS = [
     category: { name: 'Video', href: '#' },
     description:
         'Building Global Community through Hip Hop Culture',
-    date: 'Mar 10, 2020',
-    datetime: '2020-03-10',
     imageUrl:
-        'https://static1.squarespace.com/static/5b521e334cde7a741d47e768/t/5b5616f20e2e728fc44d8ecb/1620247551740/?format=1500w',
-    readingTime: '4 min'
+        'https://static1.squarespace.com/static/5b521e334cde7a741d47e768/t/5b5616f20e2e728fc44d8ecb/1620247551740/?format=1500w'
   },
   {
     title: 'Improve your customer experience',
@@ -29,11 +23,8 @@ const POSTS = [
     category: { name: 'Case Study', href: '#' },
     description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-    date: 'Feb 12, 2020',
-    datetime: '2020-02-12',
     imageUrl:
-        'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-    readingTime: '11 min'
+        'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80'
   }
 ]
 
@@ -56,7 +47,7 @@ export default function OrgAffiliations () {
               <div className='flex-shrink-0'>
                 <img className='h-48 w-full object-cover' src={post.imageUrl} alt='' />
               </div>
-              <div className='flex-1 bg-white p-6 flex flex-col justify-between'>
+              <div className='flex-1 bg-earthYellow p-6 flex flex-col justify-between'>
                 <div className='flex-1'>
                   <p className='text-sm font-medium text-glaucous'>
                     <a
