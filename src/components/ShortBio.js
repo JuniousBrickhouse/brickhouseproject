aadad4b5c2512e8adc745854c8e4f13024eba794
Logan Robinson <loganrobinson315@gmail.com>
2021-05-31 10:21:51 -0400

    screen size fixed

--- src/components/ShortBio.js
+++ src/components/ShortBio.js
@@ -11,8 +11,8 @@ const SHOTR_BIO =
 
 export default function ShortBio ({ showAnimation, handleRenderJourney }) {
   return (
-    <div className='relative py-16 bg-white'>
-      <div className='hidden absolute top-0 inset-x-0 h-1/2 bg-gray-50 lg:block' aria-hidden='true' />
+    <div className='relative py-32 bg-white'>
+      <div className='hidden absolute top-0 inset-x-0 h-screen bg-gray-50 lg:block' aria-hidden='true' />
       <div className='max-w-7xl mx-auto bg-burlyWood lg:bg-transparent lg:px-8'>
         <div className='lg:grid lg:grid-cols-12'>
           <div className='relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-4 lg:py-16 lg:bg-transparent'>
