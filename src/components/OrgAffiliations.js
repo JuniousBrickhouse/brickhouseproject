1e3456b8137460004b5549a1d76d7108049a2827
Daniel McKinney <adrmckinney@gmail.com>
2021-06-02 11:01:28 -0400

    added content for hats

--- src/components/OrgAffiliations.js
+++ src/components/OrgAffiliations.js
@@ -3,29 +3,29 @@ import urbanArtistry from './photos/logos/urbanArtistry.jpg'
 
 const POSTS = [
   {
-    title: '23rd and Hampton Community Initiatives',
+    title: '23rd and Hampton Community Initiatives, Owner',
     href: 'https://23hci.com/',
     category: { name: 'Article', href: '#' },
     description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+        'Junious founded 23rd and Hampton Community Initiatives in 2020 to offer his expertise as a consultant to organizations and individuals. Through 23rd and Hampton, Junious can be contracted for consulting in all his specialties, including Arts Administration, Program Management, Community Development, Diplomacy, Folklore, and Conflict Transformation',
     imageUrl:
     'https://img1.wsimg.com/isteam/ip/93b63956-2d59-47b1-8349-dfc27f026e57/23rd%20%26%20Hampton%20Logo%201b%20copy.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1320,h:1000,cg:true,m/cr=w:1320,h:1000,a:cc'
   },
   {
-    title: 'Next Level',
+    title: 'Next Level, Director',
     href: '#',
     category: { name: 'Video', href: '#' },
     description:
-        'Building Global Community through Hip Hop Culture',
+        'Having been with Next Level since its inception, serving as an Artist-Educator on the first trip to Senegal, Junious now serves as the Director of the program, following in the footsteps of Duke Ellington and the State Department’s Jazz Ambassadors . Junio us shapes the vision of this program and guides team members in delivering culturally sensitive and highly practical education in the areas of Artistic Excellence, Entrepreneurship, and Conflict Transformation.',
     imageUrl: nextLevel
     // 'https://static1.squarespace.com/static/5b521e334cde7a741d47e768/t/5b5616f20e2e728fc44d8ecb/1620247551740/?format=1500w'
   },
   {
-    title: 'Urban Artistry, Inc.',
+    title: 'Urban Artistry, Inc., Founder and Artistic Director',
     href: '#',
     category: { name: 'Case Study', href: '#' },
     description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+        'Upon returning to the US in 2005 following 20 years of international service in the Army and as a civilian, Junious recognized the need for an institution to preserve and promote Urban Dance through structured mentorship, professional development, and community-building. He foundedthe non-profit Urban Artistry, Inc., which has been serving the DMV area and beyond throughclasses, mentorship, performances, and annual events such as the International Soul Societyfestiva',
     imageUrl: urbanArtistry
   }
 ]
@@ -47,7 +47,7 @@ export default function OrgAffiliations () {
           {POSTS.map((post) => (
             <div key={post.title} className='flex flex-col rounded-lg shadow-lg overflow-hidden font-poppins'>
               <div className='flex-shrink-0 bg-white'>
-                <img className={`${post.title === 'Next Level' ? 'object-contain' : 'object-cover'} h-48 w-full py-2`} src={post.imageUrl} alt={post.title} />
+                <img className={`${post.imageUrl === nextLevel ? 'object-contain' : 'object-cover'} h-48 w-full py-2`} src={post.imageUrl} alt={post.title} />
               </div>
               <div className='flex-1 bg-platinum p-6 flex flex-col justify-between'>
                 <div className='flex-1'>
