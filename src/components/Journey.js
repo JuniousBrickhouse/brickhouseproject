9706b50fbbe0a217498933968a2530bddf50ec4c
Daniel McKinney <adrmckinney@gmail.com>
2021-06-03 00:37:23 -0400

    added contact form and set up the animation

--- src/components/Journey.js
+++ src/components/Journey.js
@@ -17,7 +17,7 @@ export default function Journey ({ triggerPageChangeAnimation }) {
               Junious Brickhouse
             </h3>
             <button
-              onClick={() => triggerPageChangeAnimation('landingPage')}
+              onClick={() => triggerPageChangeAnimation('')}
               type='button'
               className='inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-mediumCarmine hover:bg-sonicSilver focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
             >
