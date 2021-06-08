467c86e74022fcbdea056cce35192c5aa98655cb
Daniel McKinney <adrmckinney@gmail.com>
2021-06-08 17:05:21 -0400

    added something

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ import MessageSentModal from './MessageSentModal'
 import { Transition } from '@headlessui/react'
 import MessageErrorModal from './MessageErrorModal'
 
-const SERVICE_ID = 'service_rr6w06b'
-const TEMPLATE_ID = 'brickhouse_consulting'
-const USER_ID = 'user_x7yYaw560caoImG7WOD7y'
+const SERVICE_ID = 'safe'
+const TEMPLATE_ID = 'safe'
+const USER_ID = 'safe'
 
 export default function Contact ({ triggerPageChangeAnimation }) {
   const [renderModal, setRenderModal] = useState(false)
@@ -299,14 +299,14 @@ export default function Contact ({ triggerPageChangeAnimation }) {
                       >
                       <RefreshIcon className='h-4 w-4 mr-4 self-center animate-spin' />
                       Send
-                      </button>
+                    </button>
                     : <button
                         type='submit'
                         className='mt-2 w-full inline-flex items-center justify-center px-3 py-1.5 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-mediumCarmine hover:bg-fawn focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-mediumCarmine sm:w-auto'
                       >
                       <MailIcon className='h-6 w-6 mr-2' />
                       Send
-                      </button>}
+                    </button>}
 
                   <button
                     type='button'
