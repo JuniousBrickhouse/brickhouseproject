9fe5a23cc812ed5fe9204555f18342befa66dbec
Daniel McKinney <adrmckinney@gmail.com>
2021-06-13 00:05:28 -0400

    changed dir  again

--- src/components/journey/JourneyNextLevel.js
+++ src/components/journey/JourneyNextLevel.js
@@ -1,6 +1,6 @@
 import { CONTENT } from './journeyContent'
 // import juniousWoods from '.././photos/juniousWoods.jpeg'
-import juniousWoods from 'src/components/photos/juniousWoods.jpeg'
+import juniousWoods from '../../components/photos/juniousWoods.jpeg'
 
 export default function JourneyNextLevel () {
   return (
