0858b3cd065ec9aa4cbaab35738a3a760ab5f1e0
Daniel McKinney <adrmckinney@gmail.com>
2021-06-15 00:24:39 -0400

    removed words from videos and adjusted the padding on bottom of the 'video' title to the first video.

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -8,12 +8,12 @@ export default function Videos () {
         <div className='bg-woodGray w-full'>
           <div className='w-full mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between'>
             <div className='max-w-xl'>
-              <h2 className='text-4xl font-extrabold text-mediumCarmine font-poppins sm:text-5xl sm:tracking-tight lg:text-6xl'>
+              <h2 className='text-4xl font-extrabold text-mediumCarmine font-poppins sm:text-5xl sm:tracking-tight lg:text-6xl pb-20'>
                 Videos
               </h2>
-              <p className='mt-5 text-xl text-gray-400'>
+              {/* <p className='mt-5 text-xl text-gray-400'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. At qui illum dolorum cum voluptate? Repellat ducimus ipsa exercitationem corrupti eum saepe id! Temporibus veniam exercitationem neque excepturi voluptatem ut repudiandae!
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
