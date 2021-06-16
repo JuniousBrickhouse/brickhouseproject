07df946a1b650bb04f8f53fe6cc10f87f05562e2
Daniel McKinney <adrmckinney@gmail.com>
2021-06-16 10:16:28 -0400

    chaged bio background to extend the ynBlue and changed the mobile and sm view of the org affiliation to be all mediumCarmine background.

--- src/components/OrgAffiliations.js
+++ src/components/OrgAffiliations.js
@@ -35,7 +35,7 @@ const POSTS = [
 
 export default function OrgAffiliations ({ triggerPageChangeAnimation }) {
   return (
-    <div className='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
+    <div className='relative bg-mediumCarmine lg:bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
       <div className='absolute inset-0'>
         <div className='bg-mediumCarmine h-1/3 sm:h-2/3' />
       </div>
