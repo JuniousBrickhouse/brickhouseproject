e25364745452ef87482b618607efcb6a98accb26
Daniel McKinney <adrmckinney@gmail.com>
2021-06-16 23:57:58 -0400

    finished styling djscotty divider

--- src/components/journey/DjScottyDivider.js
+++ src/components/journey/DjScottyDivider.js
@@ -1,5 +1,5 @@
 export default function DjScottyDivider () {
   return (
-    <div className=' mx-auto px-4 sm:px-6 lg:px-8 h-full w-full bg-djScotty bg-no-repeat bg-center bg-200% sm:bg-cover parallax' aria-hidden='true' />
+    <div className=' mx-auto px-4 sm:px-6 lg:px-8 h-full w-full bg-djScotty bg-no-repeat bg-center bg-cover parallax' aria-hidden='true' />
   )
 }
