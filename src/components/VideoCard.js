3325786ed9c8ba94a952fc6b371260e5af08edcc
Daniel McKinney <adrmckinney@gmail.com>
2021-06-22 15:06:41 -0400

    changed a couple things

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -6,11 +6,6 @@ import { VIDEOS } from './Lists'
 export default function VideoCard ({ showAnimation }) {
   const [playVideo, setPlayVideo] = useState(null)
 
-  const playSelectedVideo = (idx) => {
-    console.log('idx', idx)
-    setPlayVideo(idx)
-  }
-
   return (
     <>
       <ul className='grid grid-cols-1 gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center -mt-20 px-4'>
@@ -20,12 +15,12 @@ export default function VideoCard ({ showAnimation }) {
             className='relative col-span-1 rounded-md h-full w-full aspect-w-16 aspect-h-9'
           >
             {playVideo === idx
-              ? <iframe id='ytplayer' title={video.title} src={video.videoUrl} frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen className='w-full h-full shadow-2xl rounded-md object-cover object-left' />
+              ? <iframe id='ytplayer' title={video.title} src={video.videoUrl} frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen className='shadow-2xl rounded-md object-cover object-left' />
               : <img
                   src={video.customThumbnail}
                   alt={video.title}
                   className='shadow-2xl rounded-md object-cover object-center'
-                  onClick={() => playSelectedVideo(idx)}
+                  onClick={() => setPlayVideo(idx)}
                 />}
 
           </li>
