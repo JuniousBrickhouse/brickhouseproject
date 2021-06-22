1f858263608b448c006cfebac9d494c78eb52023
Daniel McKinney <adrmckinney@gmail.com>
2021-06-22 13:15:44 -0400

    added thumbnail images to youtube videos that toggle to the full video when clicked

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,27 +1,33 @@
+import { useState } from 'react'
 import { VIDEOS } from './Lists'
 
 // video screen sizes/ratios: 640x360; 480x270; 426x240; 320x180
 
 export default function VideoCard ({ showAnimation }) {
+  const [playVideo, setPlayVideo] = useState(null)
+
+  const playSelectedVideo = (idx) => {
+    console.log('idx', idx)
+    setPlayVideo(idx)
+  }
+
   return (
     <>
-      <ul className='hidden sm:grid md:hidden lg:grid grid-cols-1 gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center -mt-20 px-4'>
+      <ul className='grid grid-cols-1 gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center -mt-20 px-4'>
         {VIDEOS.map((video, idx) => (
           <li
             key={idx}
-            className='col-span-1 bg-gray-50 rounded-lg w-max flex justify-center'
+            className='col-span-1 rounded-lg flex justify-center h-full w-full aspect-w-16 aspect-h-9'
           >
-            <iframe id='ytplayer' title={video.title} src={video.videoUrl} frameBorder='0' width='426' height='240' type='text/html' className='p-0 shadow-2xl rounded-md' />
-          </li>
-        ))}
-      </ul>
-      <ul className='grid sm:hidden md:grid lg:hidden grid-cols-1 gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center -mt-20 px-4'>
-        {VIDEOS.map((video, idx) => (
-          <li
-            key={idx}
-            className='col-span-1 bg-gray-50 rounded-lg w-max flex justify-center'
-          >
-            <iframe id='ytplayer' title={video.title} src={video.videoUrl} frameBorder='0' width='320' height='180' type='text/html' className='p-0 shadow-2xl rounded-md' />
+            {playVideo === idx
+              ? <iframe id='ytplayer' title={video.title} src={video.videoUrl} frameBorder='0' allow='accelerometer; autoPlay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen className='shadow-2xl rounded-md object-cover object-center' />
+              : <img
+                  src={video.customThumbnail}
+                  alt={video.title}
+                  className='shadow-2xl rounded-md object-contain object-center'
+                  onClick={() => playSelectedVideo(idx)}
+                />}
+
           </li>
         ))}
       </ul>
