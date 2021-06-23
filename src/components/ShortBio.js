fbc07d3e26eef1dab67a314d5bc60477179b9648
Daniel McKinney <adrmckinney@gmail.com>
2021-06-23 17:08:23 -0400

    fixed bug that wouldn't let you navigate to journey page

--- src/components/ShortBio.js
+++ src/components/ShortBio.js
@@ -155,7 +155,7 @@ export default function ShortBio ({ showAnimation, triggerPageChangeAnimation })
               </div>
               <button
                 className='inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-mediumCarmine hover:bg-redBackground focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 self-end'
-                onClick={() => triggerPageChangeAnimation('journeyPage')}
+                onClick={() => triggerPageChangeAnimation('journeyPage', 'homePage')}
               >
                 {SHOTR_BIO.journey}
               </button>
