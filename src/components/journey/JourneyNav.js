2a43a67c5ade7769259e73e48041908a992fa84f
Daniel McKinney <adrmckinney@gmail.com>
2021-06-23 17:05:07 -0400

    changed back btn on journey to say home

--- src/components/journey/JourneyNav.js
+++ src/components/journey/JourneyNav.js
@@ -17,18 +17,20 @@ export default function JourneyNav ({ triggerPageChangeAnimation, navBtnFade }) 
                   type='button'
                   className='inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-mediumCarmine hover:bg-sonicSilver focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                 >
-                  Go Back
+                  <ChevronDoubleLeftIcon className='pr-4 h-8 w-8' />
+                  Home
                 </button>
               </div>
+              {/* desktop nav button */}
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start pt-4'>
                 <div className='hidden sm:block sm:ml-6'>
                   <button
                     onClick={() => triggerPageChangeAnimation('homePage', 'journeyPage')}
                     type='button'
-                    className='inline-flex items-center px-4 py-2 border border-transparent text-xs font-medium rounded-md shadow-sm text-white bg-earthYellow hover:bg-sonicSilver focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-richBlack'
+                    className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-earthYellow hover:bg-sonicSilver focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-richBlack'
                   >
-                    <ChevronDoubleLeftIcon className='pr-4 h-8 w-8' />
-                    Go Back
+                    <ChevronDoubleLeftIcon className='pr-2 h-8 w-8' />
+                    Home
                   </button>
                 </div>
               </div>
